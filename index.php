c25ac61496f872dcbef5343ebd8c2cbdf48d5dc5
Daniel <mabadejedaniel1@gmail.com>
2020-07-13 06:33:01 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+&lt;!DOCTYPE html&gt;
+&lt;html&gt;
+&lt;body&gt;
+
+&lt;h1&gt;Plagiarized Checker!&lt;/h1&gt;
+&lt;p&gt;Welcome to itsourcecode.com free plagiarize checker&lt;/p&gt;
+&lt;!-- PrepostSEO Plagiarism Checker Widget --&gt;&lt;script type="text/javascript" src="//www.prepostseo.com/widget.js?"&gt;&lt;/script&gt;&lt;div style="text-align: center; font-size:12px; color:#333;"&gt;&lt;p&gt;&lt;a href="https://www.prepostseo.com/plagiarism-checker" target="_blank" style="color:#000;"&gt;Plagiarism checker&lt;/a&gt; provided by &lt;a href="https://www.prepostseo.com/" target="_blank"  style="color:#000;"&gt;prepostseo.com&lt;/a&gt;&lt;/p&gt;&lt;/div&gt;
+
+&lt;/body&gt;
+&lt;/html&gt;
